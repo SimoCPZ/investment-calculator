8fa8247ec5fa5449e899dbef37ea1433a5750ae6
SimoCPZ <simonecapezzani@gmail.com>
2024-01-03 11:43:23 +0100

    Lifting UserInput state

--- src/components/UserInput.jsx
+++ src/components/UserInput.jsx
@@ -1,79 +1,64 @@
-import { useState } from "react";
 
-export default function UserInput() {
+export default function UserInput({ onChange, userInput }) {
 
-    const [userInput, setUserInput] = useState({
-        initialInvestment: 10000,
-        annualInvestment: 1200,
-        expectedReturn: 6,
-        duration: 10
-    });
-
-    function handleChange(inputIdentifier, newValue) {
-        setUserInput(prevUserInput => {
-            return {
-                ...userInput,
-                [inputIdentifier]: newValue
-            };
-        });
-    }
-
-    return <section id="user-input">
-        <div className="input-group">
-            <p>
-                <label>
-                    Initial investment
-                </label>
-                <input
-                    type="number"
-                    required
-                    onChange={(event) =>
-                        handleChange('initialInvestment', event.target.value)
-                    }
-                    value={userInput.initialInvestment}
-                />
-            </p>
-            <p>
-                <label>
-                    Annual investment
-                </label>
-                <input
-                    type="number"
-                    required
-                    onChange={(event) =>
-                        handleChange('annualInvestment', event.target.value)
-                    }
-                    value={userInput.annualInvestment}
-                />
-            </p>
-        </div>
-        <div className="input-group">
-            <p>
-                <label>
-                    Expected return
-                </label>
-                <input
-                    type="number"
-                    required
-                    onChange={(event) =>
-                        handleChange('expectedReturn', event.target.value)
-                    }
-                    value={userInput.expectedReturn}
-                />
-            </p>
-            <p>
-                <label>
-                    Duration
-                </label>
-                <input
-                    type="number"
-                    required
-                    onChange={(event) =>
-                        handleChange('duration', event.target.value)
-                    }
-                    value={userInput.duration}
-                />
-            </p>
-        </div>
-    </section>;
+    return (
+        <section id="user-input">
+            <div className="input-group">
+                <p>
+                    <label>
+                        Initial investment
+                    </label>
+                    <input
+                        type="number"
+                        required
+                        onChange={(event) =>
+                            onChange('initialInvestment', event.target.value)
+                        }
+                        value={userInput.initialInvestment}
+                    />
+                </p>
+                <p>
+                    <label>
+                        Annual investment
+                    </label>
+                    <input
+                        type="number"
+                        required
+                        onChange={(event) =>
+                            onChange('annualInvestment', event.target.value)
+                        }
+                        value={userInput.annualInvestment}
+                    />
+                </p>
+            </div>
+            <div className="input-group">
+                <p>
+                    <label>
+                        Expected return
+                    </label>
+                    <input
+                        type="number"
+                        required
+                        onChange={(event) =>
+                            onChange('expectedReturn', event.target.value)
+                        }
+                        value={userInput.expectedReturn}
+                    />
+                </p>
+                <p>
+                    <label>
+                        Duration
+                    </label>
+                    <input
+                        type="number"
+                        required
+                        onChange={(event) =>
+                            onChange('duration', event.target.value)
+                        }
+                        value={userInput.duration}
+                    />
+                </p>
+            </div>
+        </section>
+    );
 }
