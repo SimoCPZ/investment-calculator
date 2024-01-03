83fa1c7cb698891f8d2169eb3ccb2d75f689fe86
SimoCPZ <simonecapezzani@gmail.com>
2024-01-03 10:57:10 +0100

    Add UserInput component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
 import Header from "./components/Header.jsx"
+import UserInput from "./components/UserInput.jsx"
 
 function App() {
   return (
-    <Header />
-  )
+    <>
+      <Header />
+      <UserInput />
+      {/* Results go here */}
+    </>
+  );
 }
 
 export default App
